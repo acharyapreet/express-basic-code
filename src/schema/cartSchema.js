252582bd186422712f98c18beea8990cb4067b85
Preet Acharya <acharyapreet2@gmail.com>
2025-07-18 17:20:55 +0530

    added cartSchema

--- src/schema/cartSchema.js
+++ src/schema/cartSchema.js
@@ -21,8 +21,10 @@ const cartSchema = new mongoose.Schema({
             }
         }
     ],
-    totalPrice : {
-        type : Number,
-        default : 0
-    }
+},{
+    timestamps : true
 })
+
+const Cart = mongoose.model('Cart',cartSchema);
+
+module.exports = Cart
